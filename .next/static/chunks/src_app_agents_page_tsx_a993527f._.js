3461b3d84bcabb3245ad028b0fd8bee86ef3e8d9
elimike10 <150400832+elimike10@users.noreply.github.com>
2025-07-23 15:07:09 +0300

    Add .next/static/chunks/src_app_agents_page_tsx_a993527f._.js 1753272429059

---
+++ .next/static/chunks/src_app_agents_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_c50fab3f._.js"
+],
+    source: "dynamic"
+});
